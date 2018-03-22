d2dff496c751e1e658bbbc64e3e247f6a2e1b43d
corina.pip <capreoara6@gmail.com>
2018-03-22 14:35:48 +0200

    Add suport for cross environment testing.

---
+++ src/test/java/firstSteps/FirstTestWithSelenium.java
@@ -0,0 +1,28 @@
+package firstSteps;
+
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+import utils.BrowserUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+@ContextConfiguration("/application-context.xml")
+public class FirstTestWithSelenium extends AbstractTestNGSpringContextTests{
+    @Value("${homepageurl}") private String homepageUrl;
+
+    @Test
+    public void a() throws IOException, InterruptedException {
+        System.out.println("homepageUrl = " + homepageUrl);
+        BrowserUtil browserUtil = new BrowserUtil();
+        WebDriver driver = browserUtil.startBrowser();
+        driver.get("http://www.google.com");
+        driver.get(new File(homepageUrl).getAbsolutePath());
+        TimeUnit.SECONDS.sleep(15);
+        browserUtil.quit(driver);
+    }
+}
