8441876ca1e09069c18f82863b9a72fd1d500636
corina.pip <capreoara6@gmail.com>
2018-03-27 14:34:58 +0300

    Add support for cross environment testing and test suite file.

---
+++ src/main/java/utils/SetupHelper.java
@@ -0,0 +1,8 @@
+package utils;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+@ContextConfiguration("/application-context.xml")
+public class SetupHelper extends AbstractTestNGSpringContextTests {
+}
--- src/test/java/firstTests/FirstTestWithSelenium.java
+++ src/test/java/firstTests/FirstTestWithSelenium.java
@@ -1,26 +1,23 @@
-package firstSteps;
+package firstTests;
 
 import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 import utils.BrowserUtil;
+import utils.SetupHelper;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-@ContextConfiguration("/application-context.xml")
-public class FirstTestWithSelenium extends AbstractTestNGSpringContextTests{
+public class FirstTestWithSelenium extends SetupHelper{
     @Value("${homepageurl}") private String homepageUrl;
 
     @Test
-    public void a() throws IOException, InterruptedException {
+    public void firstTest() throws IOException, InterruptedException {
         System.out.println("homepageUrl = " + homepageUrl);
         BrowserUtil browserUtil = new BrowserUtil();
         WebDriver driver = browserUtil.startBrowser();
-        driver.get("http://www.google.com");
         driver.get(new File(homepageUrl).getAbsolutePath());
         TimeUnit.SECONDS.sleep(5);
         browserUtil.quit(driver);
